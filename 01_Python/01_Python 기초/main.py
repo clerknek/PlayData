3cc5a7669ea5c7bc825b469637c7abb3a341ff0c
jongminjeon <71575861+jongminjeon@users.noreply.github.com>
2023-01-03 09:19:15 +0900

    Path(2023-01-02)

---
+++ 01_Python/01_Python 기초/main.py
@@ -0,0 +1,21 @@
+a = 10
+b = 20
+c = a + b
+print(c)
+
+def greeting(name):
+    pass
+
+import my_module
+import my_package.calculator
+
+result = my_package.calculator.multipy(a, b)
+print("곱셈결과:", result)
+
+
+r = my_module.greeting("홍길동")
+print(r)
+p = my_module.Person('이순신', 30)
+print(p)
+
+# 명령프롬프트(터미널)에서 python main.py
---
+++ 01_Python/01_Python 기초/test.py
@@ -0,0 +1,5 @@
+print(1+1)
+print(2*100)
+a = 10
+print(a)
+print(a + 30)
