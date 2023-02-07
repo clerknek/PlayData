80df1f61a6ec92e948f030b2ff6556e1d5bbc77a
clerknek <71575861+clerknek@users.noreply.github.com>
2023-02-07 17:52:18 +0900

    Voting updated

--- 05_MachineLearning/metrics.py
+++ 05_MachineLearning/metrics.py
@@ -1,8 +1,11 @@
-# 평가지표 저장 모듈
-__version__= 1.0
+
+# 평가
+__version__= 1.1
 
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, recall_score, precision_score, f1_score, accuracy_score
+from sklearn.metrics import (confusion_matrix, ConfusionMatrixDisplay,
+                             recall_score, precision_score, f1_score,
+                             accuracy_score, roc_auc_score, average_precision_score)
 
 def plot_confusion_matrix(y, pred, title=None):
     """
@@ -38,3 +41,15 @@ def print_metrics_classification(y, pred, title=None):
     print("재현율/민감도(recall):", recall_score(y, pred))
     print("정밀도(precision):", precision_score(y, pred))
     print("F1-score:", f1_score(y, pred))
+
+def print_metrics_classification2(y, pred_pos_proba, title=None):
+    """
+    Average Precision Score와 roc-auc score를 출력
+    [parameter]
+        y: ndarray - 정답의 label
+        pred_pos_proba: ndarray - 모델이 추론한 positive(양성)의 확률
+    """
+    if title:
+        print(title)
+    print('Average Precision:', average_precision_score(y, pred_pos_proba))
+    print('roc_auc', roc_auc_score(y, pred_pos_proba))
